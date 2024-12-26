c95e4e4c87ade3dbb47940cca8122c501aa4cbc2
RenatMirzayev <renadmirzoyev@gmail.com>
2024-12-26 12:23:41 +0400

    betty

--- shel.c
+++ shel.c
@@ -1,59 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
-    pid_t pid;
-    int status;
+    /* Printing "Hello, World!" to the console */
+    printf("Hello, World!\n");
 
-    while (1)
-    {
-        printf("#cisfun$ ");
-	
-	nread = getline(&line, &len, stdin);
-        if (nread == -1)
-	{
-            printf("\n");
-            break;
-        }
-	
-	if (line[nread - 1] == '\n')
-            line[nread - 1] = '\0';
-
-        pid = fork();
-
-        if (pid == -1)
-        {
-            perror("fork");
-            free(line);
-            exit(EXIT_FAILURE);
-        }
-        if (pid == 0) 
-	{
-            char *args[2];
-            args[0] = line;
-            args[1] = NULL;
-            
-	    if (execve(line, args, NULL) == -1)
-            {
-                perror("execve");
-                free(line);
-                exit(EXIT_FAILURE);
-            }
-        }
-        else 
-	{
-            wait(&status);
-	}
-    }
-
-    free(line);
-    return 0;
+    return (0);
 }
